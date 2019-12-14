58669a94c75c3e5981592a3ccf12d55c6ab1ae68
thotasp <sreepallavi.thota@lcginc.com>
2019-12-14 02:12:59 -0500

    initial commit

---
+++ ResearcherInfoService/ResearcherInfoService/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace ResearcherInfoService
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            //comment
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
